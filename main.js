b39921588de66a01dfc307f59de1b9542eadc156
Anas-Alnabahin <anasnb2002nb@gmail.com>
2022-12-11 01:23:19 +0200

    Fetching data to the main todo page and adjusting styling file and js code

--- main.js
+++ main.js
@@ -4,6 +4,61 @@ const addBtn = document.getElementById('vector');
 const xBtn = document.getElementById('xMark');
 const tBtn = document.getElementById('tMark');
 
+
+let counter = 0;
+
+let outerDiv = `  
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 26px 34px;
+gap: 12px;
+width: 445px;
+height: 171px;
+background: #ffffff;
+box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+border-radius: 8px;
+flex: none;
+
+flex-grow: 0;`;
+
+let innerDiv = `  
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 12px;
+width: 377px;
+height: 119px;
+flex: none;
+order: 0;
+flex-grow: 0;`;
+
+let theH4 = `  
+width: 221px;
+height: 29px;
+font-family: "Inter";
+font-style: normal;
+font-weight: 800;
+font-size: 24px;
+line-height: 29px;
+color: #374151;
+flex: none;
+order: 0;
+flex-grow: 0;`;
+
+let theP = `  
+width: 377px;
+height: 41px;
+font-family: "Inter";
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 19px;
+color: #6b7280;
+flex: none;
+order: 1;
+flex-grow: 0;`;
 addBtn.onclick = () => {
     document.documentElement.scrollTop = 0;
     addBtn.style.display = 'none';
@@ -12,3 +67,29 @@ addBtn.onclick = () => {
     xBtn.style.display = 'block';
     tBtn.style.display = 'block';
 };
+
+window.onload = () => {
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status === 200){
+            const res = JSON.parse(req.responseText);
+            for(let x = 0; x < res.length; x++){
+                console.log(res[x].id);
+                todoPage.innerHTML += `<div style='${outerDiv} order: ${counter};'>
+                                            <div style='${innerDiv}'>
+                                                <h4 style='${theH4} overflow: hidden;'>
+                                                    ${res[x].title}
+                                                </h4>
+                                                <p style='${theP}'>
+                                                    ${res[x].body}
+                                                </p>
+                                            </div>
+                                      </div>`
+                counter++;
+            }
+        }
+    }
+        req.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+        req.send();
+    };
